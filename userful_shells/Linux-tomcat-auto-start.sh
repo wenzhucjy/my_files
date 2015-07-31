156b3d45693b6b86242225241e90a4de373dc467
wenzhu <wenzhucjy@gmail.com>
2015-07-31 13:47:47 +0800

    modify Linux-tomcat-auto-start.sh

--- userful_shells/Linux-tomcat-auto-start.sh
+++ userful_shells/Linux-tomcat-auto-start.sh
@@ -1,37 +1,61 @@
 #!/bin/bash
-#
-# chkconfig: 2345 20 80
-# description: Auto-starts tomcat
-# /etc/init.d/tomcat
-# chmod 755 tomcat
-# cp tomcat /etc/init.d/
-# ln -s /etc/init.d/tomcat /etc/rc.d/init.d/tomcat
-# chkconfig --add tomcat
-# chkconfig --level 2345 tomcat on 
-# init script for tomcat precesses 
+### BEGIN INIT INFO
+# Provides:          tomcat8
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Start Tomcat.
+# Description:       Start the Tomcat servlet engine,init script for Tomcat precesses.
+### END INIT INFO
 
-source /etc/profile
+TOMCAT_USER=web
+TOMCAT_GROUP=web
+TOMCAT_HOME=/data/apache
+TOMCAT_BIN=$TOMCAT_HOME/bin
+TOMCAT_TEMP=$TOMCAT_HOME/temp
+TOMCAT_LOCK=/var/run/tomcat.lock
+TOMCAT_UMASK=002
 
-CATALINA_HOME="/usr/local/tomcat-7.0.47"
-case $1 in
-start)
-        echo -n $"Starting Tomcat"
-        $CATALINA_HOME/bin/startup.sh
-        echo " done"
+if [ `id -u` -ne 0 ]; then
+        echo "You need root or sudo privileges to run this script"
+        exit 1
+fi
+
+start_sams() {
+        if [ -f $TOMCAT_LOCK ];then
+                echo ' * tomcat has already been started or has problems'
+                exit 1
+        fi
+        /usr/local/bin/start-stop-daemon --start -u "$TOMCAT_USER" -g "$TOMCAT_GROUP" \
+                -c "$TOMCAT_USER" -d "$TOMCAT_TEMP" \
+                -k "$TOMCAT_UMASK" -x "$TOMCAT_BIN/startup.sh" > /dev/null && \
+                echo " * tomcat starts successfully" &&  touch $TOMCAT_LOCK
+}
+
+stop_sams() {
+        if [ ! -f $TOMCAT_LOCK ];then
+                echo ' * tomcat has already been stopped or has problems'
+                exit 1
+        fi
+        /usr/local/bin/start-stop-daemon --stop -u "$TOMCAT_USER" -g "$TOMCAT_GROUP" \
+                -c "$TOMCAT_USER" -d "$TOMCAT_TEMP" \
+                -k "$TOMCAT_UMASK" "$TOMCAT_BIN/shutdown.sh" && \
+                echo " * tomcat stops successfully" && rm -f $TOMCAT_LOCK
+}
+
+case "$1" in
+        start)
+                start_sams
         ;;
-stop)
-        echo -n $"Stopping Tomcat"
-        $CATALINA_HOME/bin/shutdown.sh
-        echo " done"
+        stop)
+                stop_sams
         ;;
-restart)
-        echo $"Restarting Tomcat"
-        $0 stop
-        sleep 1
-        $0 start
-        echo " done"
+        restart)
+                stop_sams
+                sleep 10
+                start_sams
         ;;
-*)
-        echo $"Usage: $0 {start|stop|restart}"
+        *)
+                echo ' * Usage: Service tomcat {start|stop|restart}'
+                exit 1
         ;;
 esac
